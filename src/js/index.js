30ac8bc2dd7401f087b1cfa7cd4bf38295712c94
xm94630 <xm94630@126.com>
2018-11-08 23:20:04 +0800

    简化项目

--- src/js/index.js
+++ src/js/index.js
@@ -39,8 +39,6 @@ var w = 750;
 var h = 1200;
 var mySound;
 var progress;
-var nPage = 1; //第几页
-var totalPage = 38;
 var myTicker = function(){}
 var app = new PIXI.Application({width:w,height: h,backgroundColor:0xffffff});
 app.view.style.position = "absolute";
@@ -117,65 +115,6 @@ function getAllMaterial(res){
       });
       return mc;
     },
-    nextBtnMC:function(){
-      var sourceArr = characterAnimation['button.json']['viewBtn'];
-      var frameArr=[];
-      for (var i = 0; i < sourceArr.length; i++) {
-        frameArr.push(PIXI.Texture.fromFrame( sourceArr[i] ));
-      }
-      var mc = new PIXI.extras.AnimatedSprite(frameArr);
-      mc.x = w-mc.width/2-20;
-      mc.y = h-mc.height/2-20;
-      mc.anchor.set(0.5);
-      mc.gotoAndStop(2); 
-      mc.interactive = true;
-      mc.buttonMode = true;
-      mc.scale.x = mc.scale.y = 1;
-      mc.on('pointerdown', function(){
-        nPage++;
-        if(nPage>=totalPage){
-          stage2.getChildByName('nextBtn').visible=false;
-        }else{
-          stage2.getChildByName('prevBtn').visible=true;
-          stage2.getChildByName('nextBtn').visible=true;
-        }
-        var bookMC = stage2.getChildByName('book');
-        bookMC.removeChildren(0, bookMC.children.length);
-        bookMC.addChild(list.pageMC(nPage));
-      });
-      mc.name="nextBtn"
-      return mc;
-    },
-    prevBtnMC:function(){
-      var sourceArr = characterAnimation['button.json']['viewBtn'];
-      var frameArr=[];
-      for (var i = 0; i < sourceArr.length; i++) {
-        frameArr.push(PIXI.Texture.fromFrame( sourceArr[i] ));
-      }
-      var mc = new PIXI.extras.AnimatedSprite(frameArr);
-      mc.x = mc.width/2+20;
-      mc.y = h-mc.height/2-20;
-      mc.anchor.set(0.5);
-      mc.gotoAndStop(1); 
-      mc.interactive = true;
-      mc.buttonMode = true;
-      mc.scale.x = mc.scale.y = 1;
-      mc.on('pointerdown', function(){
-        nPage--;
-        if(nPage<=1){
-          stage2.getChildByName('prevBtn').visible=false;
-        }else{
-          stage2.getChildByName('prevBtn').visible=true;
-          stage2.getChildByName('nextBtn').visible=true;
-        }
-        var bookMC = stage2.getChildByName('book');
-        bookMC.removeChildren(0, bookMC.children.length);
-        bookMC.addChild(list.pageMC(nPage));
-      });
-      mc.name="prevBtn"
-      mc.visible = false;
-      return mc;
-    },
     btn:function(){
       var txt = new PIXI.Text('游戏开始',{
         fontSize: 60,
@@ -194,30 +133,15 @@ function getAllMaterial(res){
     bookMC:function(){
       var mc = new PIXI.Container();
       mc.name = "book";
-      mc.addChild(list.pageMC(1))
+      mc.addChild(list.pageMC())
       return mc;
     },
-    pageMC:function(nPage){
-      var mc = PIXI.Sprite.fromImage("page"+nPage);
-      mc.name = "page"+nPage;
+    pageMC:function(){
+      var mc = PIXI.Sprite.fromImage("bg2");
+      mc.name = "bg2";
       mc.width = w; 
       mc.height = h; 
       return mc;
-    },
-    pageNumberMC:function(){
-      var txt = new PIXI.Text( nPage+"/"+totalPage,{
-        fontSize: 30,
-        fill: 0xc3592f,
-        align: 'left',
-        fontFamily:'monogram',
-      });
-      txt.anchor.set(.5)
-      txt.x = w/2;
-      txt.y = 1100;
-      txt.interactive = true;
-      txt.buttonMode = true;
-      txt.name="pageNumber"
-      return txt;
     },
     homeBtnMC:function(){
       var mc = PIXI.Sprite.fromImage("icon-exit.png");
@@ -231,13 +155,6 @@ function getAllMaterial(res){
       mc.on('pointerdown', function(){
         app.stage.getChildByName('stage1').visible = true;
         app.stage.getChildByName('stage2').visible = false;
-        nPage=1;
-        stage2.getChildByName('prevBtn').visible=false;
-        stage2.getChildByName('nextBtn').visible=true;
-        //复位
-        var bookMC = stage2.getChildByName('book');
-        bookMC.removeChildren(0, bookMC.children.length);
-        bookMC.addChild(list.pageMC(nPage));
       });
       return mc;
     }
@@ -297,20 +214,12 @@ function stage1_layout(res){
 //场景布局2
 function stage2_layout(res){   
   const{
-    prevBtnMC,
-    nextBtnMC,
-    soundBtnMC,
     bookMC,
-    pageMC,
-    pageNumberMC,
     homeBtnMC,
   } = getAllMaterial(res);
   stage2.removeChildren(0, stage2.children.length);
   stage2.addChild(
     bookMC(),
-    nextBtnMC(),
-    prevBtnMC(),
-    pageNumberMC(),
     homeBtnMC(),
   );
 }
@@ -323,9 +232,7 @@ function stage0_ticker(delta){
   stage0.getChildByName('progressText').text = progress;
 }
 function stage1_ticker(delta){}
-function stage2_ticker(delta){
-  stage2.getChildByName('pageNumber').text = nPage+" / "+totalPage;
-}
+function stage2_ticker(delta){}
 
 
 
@@ -349,44 +256,7 @@ PIXI.loader
       .add("bgmSound","./sounds/bgm.mp3")
       .add("btn", "./img/btn.json")
       .add("buttons", "./img/buttons.json")
-      .add("page1", "./img/1.jpg")
-      .add("page2", "./img/2.jpg")
-      .add("page3", "./img/3.jpg")
-      .add("page4", "./img/4.jpg")
-      .add("page5", "./img/5.jpg")
-      .add("page6", "./img/6.jpg")
-      .add("page7", "./img/7.jpg")
-      .add("page8", "./img/8.jpg")
-      .add("page9", "./img/9.jpg")
-      .add("page10", "./img/10.jpg")
-      .add("page11", "./img/11.jpg")
-      .add("page12", "./img/12.jpg")
-      .add("page13", "./img/13.jpg")
-      .add("page14", "./img/14.jpg")
-      .add("page15", "./img/15.jpg")
-      .add("page16", "./img/16.jpg")
-      .add("page17", "./img/17.jpg")
-      .add("page18", "./img/18.jpg")
-      .add("page19", "./img/19.jpg")
-      .add("page20", "./img/20.jpg")
-      .add("page21", "./img/21.jpg")
-      .add("page22", "./img/22.jpg")
-      .add("page23", "./img/23.jpg")
-      .add("page24", "./img/24.jpg")
-      .add("page25", "./img/25.jpg")
-      .add("page26", "./img/26.jpg")
-      .add("page27", "./img/27.jpg")
-      .add("page28", "./img/28.jpg")
-      .add("page29", "./img/29.jpg")
-      .add("page30", "./img/30.jpg")
-      .add("page31", "./img/31.jpg")
-      .add("page32", "./img/32.jpg")
-      .add("page33", "./img/33.jpg")
-      .add("page34", "./img/34.jpg")
-      .add("page35", "./img/35.jpg")
-      .add("page36", "./img/36.jpg")
-      .add("page37", "./img/37.jpg")
-      .add("page38", "./img/38.jpg")
+      .add("bg2", "./img/bg2.jpg")
       .load(setup)
       .onProgress.add((myLoader,res) => {
         progress = 'Loading...'+ Math.round(myLoader.progress) +'%';
@@ -400,19 +270,23 @@ PIXI.loader
  * 游戏主体逻辑部分                                                    *
  ********************************************************************/
 function setup(xxx,res) {
-
+  //声音
   mySound = res.bgmSound.sound;
   mySound && mySound.play();
 
+  //获取全部影片剪辑
   const{soundBtnMC} = getAllMaterial(res);
+  
   //场景布局（创建容器）
   stage1_layout(res);
   stage2_layout(res);
+  
   //舞台显示 (容器挂载)
   app.stage.removeChild(app.stage.getChildByName('stage0')); //移除（后续不会再用）
   app.stage.addChild(stage1,stage2,soundBtnMC());
   app.stage.getChildByName('stage1').visible = true;
   app.stage.getChildByName('stage2').visible = false;
+
   //使用场景对应的ticker
   myTicker = stage2_ticker;
 }
